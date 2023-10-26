9be4a17a70613606b23ee9c77513bf64d16cf748
rianwp <rianwp2015@gmail.com>
2023-10-27 02:59:30 +0700

    adding delete image

---
+++ app/api/lapangan/jenis/images/[id]/route.ts
@@ -0,0 +1,70 @@
+import auth from "@/lib/auth"
+import { prisma } from "@/lib/db"
+import { IdParamsInterface } from "@/types/params"
+import { NextRequest, NextResponse } from "next/server"
+
+export const DELETE = async (
+	req: NextRequest,
+	{ params }: IdParamsInterface
+) => {
+	const id = params.id
+	const user = await auth(req, "admin")
+	if (!user.success) {
+		return NextResponse.json(
+			{
+				success: false,
+				message: user.message,
+			},
+			{
+				status: user.status,
+			}
+		)
+	}
+
+	try {
+		const findId = await prisma.image.findFirst({
+			where: {
+				id,
+			},
+		})
+
+		if (!findId) {
+			return NextResponse.json(
+				{
+					success: false,
+					message: "Image dengan id tersebut tidak ditemukan",
+				},
+				{
+					status: 404,
+				}
+			)
+		}
+
+		await prisma.image.delete({
+			where: {
+				id,
+			},
+		})
+
+		return NextResponse.json(
+			{
+				success: true,
+				message: "Image sukses dihapus",
+			},
+			{
+				status: 200,
+			}
+		)
+	} catch (err) {
+		const error = err as Error
+		return NextResponse.json(
+			{
+				success: false,
+				message: error.message ?? "Terjadi kesalahan pada server",
+			},
+			{
+				status: 500,
+			}
+		)
+	}
+}
