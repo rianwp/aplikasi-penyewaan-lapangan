87d5d11c904ba36f54fe6021f66ce6199cfc98b7
rianwp <rianwp2015@gmail.com>
2023-11-11 13:11:39 +0700

    removing seed.ts

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,19 +1,19 @@
-import { PrismaClient } from "@prisma/client"
-import adminData from "../lib/adminData"
+// import { PrismaClient } from "@prisma/client"
+// import adminData from "../lib/adminData"
 
-const prisma = new PrismaClient()
+// const prisma = new PrismaClient()
 
-async function main() {
-	await prisma.user.create({
-		data: adminData,
-	})
-}
+// async function main() {
+// 	await prisma.user.create({
+// 		data: adminData,
+// 	})
+// }
 
-main()
-	.catch((e) => {
-		console.error(e)
-		process.exit(1)
-	})
-	.finally(async () => {
-		await prisma.$disconnect()
-	})
+// main()
+// 	.catch((e) => {
+// 		console.error(e)
+// 		process.exit(1)
+// 	})
+// 	.finally(async () => {
+// 		await prisma.$disconnect()
+// 	})
